bcd0ad3d7ba5227a65815f9e22b7a7d484086e57
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-08-09 10:55:28 -0700

    working demo

--- sample.py
+++ sample.py
@@ -17,24 +17,14 @@ draw = ImageDraw.Draw(imgpil)
 font = ImageFont.truetype(".\AbyssinicaSIL-Regular.ttf", 32)
 
 for x, i in enumerate(text.splitlines()):
-    
+            
+            if x != 0 and len(i.split()) == 12:
+                i = i.split()
+                x,y,w,h = int(i[6]),int(i[7]),int(i[8]),int(i[9])
+                cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255),1)
 
-    if x != 0 and len(i.split()) == 12:
-        i = i.split()
-        # print(int(int(i[7]))
-
-        x,y,w,h = int(i[6]),int(i[7]),int(i[8]),int(i[9])
-        cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255),1)
-        draw.text((x,y), i[11], fill=(0,0,255,0), font=font)
-        # cv2.putText(img, i[11], (x,y), cv2.FONT_HERSHEY_COMPLEX, 1, (100,0,100), 1)
-
-
-img2 = np.array(imgpil)
 
 cv2.imshow("real", img)
-cv2.imshow("pred", img2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-# print(text)
